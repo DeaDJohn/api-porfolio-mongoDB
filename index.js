4325251cf0f7feb20b27b90c1079a8aa25e57816
CPU casa <juanjofb86@gmail.com>
2018-07-17 19:05:02 +0200

    Conectar el proyecto a la base de datos de mongoDB atraves de mongoose.

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/portafolio')
+    .then(() => {
+        console.log('Conexion a la base de datos establecida con éxito')
+    })
+    .catch(err => console.log(err));
