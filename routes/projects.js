01307052fbc01fbb457f8e99c3b2fee363daa6f3
Juanjo Fernandez <juanjofb86@gmail.com>
2018-07-21 10:00:03 +0200

    Añadido el metodo getImageFile con la ruta /get-image/:image

--- routes/projects.js
+++ routes/projects.js
@@ -20,5 +20,6 @@ router.get('/projects', ProjectController.getProjects);
 router.post('/project/:id?', ProjectController.updateProject);
 router.delete('/project/:id?', ProjectController.deleteProject);
 router.post('/upload-image/:id?', multipartMiddleware, ProjectController.uploadImage);
+router.get('/get-image/:image', ProjectController.getImageFile);
 
 module.exports = router;
